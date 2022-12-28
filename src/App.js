17686363d520c49cba17ae8d50811b66ac850fa0
yashpurkar <yashpurkar7079@gmail.com>
2022-12-28 23:36:01 +0530

    done JSX

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <img src="#" alt="" style={{ width: "100px", height: "100px" }} />
+        <p>Yash</p><hr />
+        <div>
+          <div>
+            <small>Articles</small>
+            <p>27</p>
+          </div>
+          <hr />
+          <div>
+            <small>Followers</small>
+            <p>444</p>
+          </div>
+          <hr />
+          <div>
+            <small>Rating</small>
+            <p>8.9</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
