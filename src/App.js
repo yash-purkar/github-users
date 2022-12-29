e22d01d498c9de2368e2c548b4a4643375460375
yashpurkar <yashpurkar7079@gmail.com>
2022-12-29 11:33:03 +0530

    style: updated style

--- src/App.js
+++ src/App.js
@@ -16,39 +16,42 @@ function App() {
   }, [])
 
   return (
-    <div className="App">
-      {
-        users.map((currElem) => {
-          return (
-            <div className='container'>
-              <div className='header'>
-                <img src={currElem.avatar_url} alt="" style={{ width: "100px", height: "100px" }} className="img" />
-                <h2>{currElem.login}</h2>
-              </div>
-              <div className='main'>
-                <div className='three-section-div'>
-                  <div className='sections'>
-                    <small>Followers</small>
-                    <p>1000</p>
-                  </div>
+    <>
+      <h1>Github Users</h1>
+      <div className="App">
+        {
+          users.map((currElem) => {
+            return (
+              <div className='container'>
+                <div className='header'>
+                  <img src={currElem.avatar_url} alt="" style={{ width: "100px", height: "100px" }} className="img" />
+                  <h2>{currElem.login}</h2>
+                </div>
+                <div className='main'>
+                  <div className='three-section-div'>
+                    <div className='sections'>
+                      <small>Followers</small>
+                      <p>10000</p>
+                    </div>
 
-                  <div className='sections'>
-                    <small>Following</small>
-                    <p>9000</p>
-                  </div>
+                    <div className='sections'>
+                      <small>Following</small>
+                      <p>2000</p>
+                    </div>
 
-                  <div className='sections'>
-                    <small>Rating</small>
-                    <p>8.9</p>
+                    <div className='sections'>
+                      <small>Rating</small>
+                      <p>8.9</p>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          )
-        })
-      }
+            )
+          })
+        }
 
-    </div>
+      </div>
+    </>
   );
 }
 
